49fa86771acaa3acbe3163b9bcbd008360588770
JANO <77622056+Devjano@users.noreply.github.com>
2021-09-15 09:34:55 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,47 @@
+module.exports = {
+    defaultPrefix: 'j!',
+    ownerID: '681553671364018196',
+    _limits: 'The following are defaults.',
+    adminCanChangeLimits: true,
+    limits: {
+        user_removals: {
+            per_minute: 1,
+            per_hour: 1
+        },
+        role_creations: {
+            per_minute: 1,
+            per_hour: 1
+        },
+        channel_creations: {
+            per_minute: 1,
+            per_hour: 1
+        },
+        role_deletions: {
+            per_minute: 1,
+            per_hour: 1
+        },
+        channel_deletions: {
+            per_minute: 1,
+            per_hour: 1
+        },
+        unbans: {
+            per_minute: 1,
+            per_hour: 1
+        
+    },
+        emoji_creations: {
+            per_minute: 1,
+            per_hour: 1
+        },
+        emoji_deletions: {
+            per_minute: 1,
+            per_hour: 1
+        }
+    },
+    
+    
+    _config: 'The following are defaults.',
+    config: {
+        _null: 'No options to configure currently.'
+    }
+};
