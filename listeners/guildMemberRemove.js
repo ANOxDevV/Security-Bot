234e6e9c136ee3571d574e0d7430d2cf9b2ee460
ANOxDevV <88611334+ANOxDevV@users.noreply.github.com>
2021-12-26 13:15:13 +0300

    Update guildMemberRemove.js

--- listeners/guildMemberRemove.js
+++ listeners/guildMemberRemove.js
@@ -2,7 +2,7 @@ const { Listener } = require('discord-akairo');
 
 module.exports = class GuildMemberRemoveListener extends Listener {
     constructor() {
-        super('guildMemberRemove', {
+        super('guildMemberRemove', { 
             emitter: 'client',
             event: 'guildMemberRemove'
         });
